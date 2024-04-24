68d6e678fbce2594438473bfca2297ee9fd01ab1
ttn1401 <120162206+ttn1401@users.noreply.github.com>
2024-04-24 16:49:34 +0700

    backend

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require ("cookie-parser");
+const app = express();
+
+app.listen 
